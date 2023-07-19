1c00a73f0d2371e6c258b1340ee4c20fe59b9293
yasminat24-meet <139688683+yasminat24-meet@users.noreply.github.com>
2023-07-19 18:17:19 +0300

    done

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -10,32 +10,34 @@ app = Flask(  # Create a flask app
 
 username = "yasmin"
 password = "123"
-facebook_friends=["sarah","saeed","ward", "George", "Fouad", "nada"]
+facebook_friends=["sarah","saeed","ward", "George", "Fouad", "nada","waseem"]
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        return 'You just made a GET request!'
-        username2=request.form['username']
-        password2=request.form['password']
+	if request.method == 'POST':
+		#return 'You just made a post request!'
+		username2=request.form['username']
+		password2=request.form['password']
 
-        if username2==username and password2==password:
-        	 return redirect(url_for('home.html'))
-    else :
-    	return render_template('login.html')
-    	print("try again")
-  
+		if username2==username and password2==password:
+			return redirect(url_for('home_page'))
+		else:
+			return render_template('login.html')
+
+	else:
+		return render_template('login.html')
+
 @app.route('/home')
 def home_page():
-  	return render_template('home.html',p= facebook_friends)
+	return render_template('home.html',p= facebook_friends)
 
 @app.route('/friend_exists/<string:name>')
 def friend(name):
 		if name in facebook_friends:
-    	result=True
+			result=True
 		else:
 			result=False
-return render_template('friend_exists.html',name=name,result=result)
+		return render_template('friend_exists.html',name=name,result=result)
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
